138e5fc10bf274391e5c241acc12e1fe2a1c57e3
dieAte <beate.fiss@indarium.de>
2021-11-15 12:41:18 +0100

    changed links from angularx-qrcode to ubirch/angularx-qrcode

--- projects/demo-app/src/app/app.module.ts
+++ projects/demo-app/src/app/app.module.ts
@@ -1,7 +1,7 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
-import { QRCodeModule } from 'angularx-qrcode';
+import { QRCodeModule } from '../../../../dist/angularx-qrcode/lib/angularx-qrcode.module';
 import { AppComponent } from './app.component';
 
 @NgModule({
